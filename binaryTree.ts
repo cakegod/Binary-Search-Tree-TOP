2b6aa1a4e233e5fe5fd070addc1bd6978b82a256
Cake <cakemancerr@gmail.com>
2022-08-10 21:21:04 +0200

    Fixed delete method

--- binaryTree.ts
+++ binaryTree.ts
@@ -1,7 +1,7 @@
 class TreeNode {
   value: number;
-  left: TreeNode;
-  right: TreeNode;
+  left: TreeNode | null;
+  right: TreeNode | null;
 
   constructor(value: number) {
     this.value = value;
@@ -17,97 +17,119 @@ class Tree {
     this.root = this.buildTree(arr);
   }
 
-  buildTree(arr: number[] = [], start = 0, end = arr.length - 1): TreeNode | null {
-
+  buildTree(
+    arr: number[] = [],
+    start = 0,
+    end = arr.length - 1,
+  ): TreeNode | null {
     if (arr.length === 0) {
-      return null
+      return null;
     }
 
     if (start > end) {
-      return null
+      return null;
     }
 
-    const middle = Math.floor((start + end) / 2)
-    const node = new TreeNode(arr[middle])
+    const middle = Math.floor((start + end) / 2);
+    const node = new TreeNode(arr[middle]);
 
     node.left = this.buildTree(arr, start, middle - 1);
-    node.right = this.buildTree(arr, middle + 1, end)
+    node.right = this.buildTree(arr, middle + 1, end);
 
     return node;
   }
 
-  insert(value: number, position?: TreeNode | null) {
-
-    if (position === undefined) {
-      position = this.root;
+  insert(value: number, node?: TreeNode | null) {
+    if (node === undefined) {
+      node = this.root;
     }
 
-    if (position === null) {
-      return new TreeNode(value)
+    if (node === null) {
+      return new TreeNode(value);
     }
 
-    if (position.value < value) {
-      position.left = this.insert(value, position.left)
-    } else if (position.value > value) {
-      position.right = this.insert(value, position.right)
+    if (node.value < value) {
+      node.left = this.insert(value, node.left);
+    } else if (node.value > value) {
+      node.right = this.insert(value, node.right);
     }
 
-    return position
+    return node;
   }
 
-  deleteNode(value, position) {
-
-    if (position === undefined) {
-      position = this.root
+  deleteNode(value, node?) {
+    // Set initial node to root
+    if (node === undefined) {
+      node = this.root;
     }
 
-    if (position === null) {
-      return null
+    if (node === null) {
+      return null;
     }
 
-    if (position.value > value) {
-      position.left = this.deleteNode(value, position.left)
-    } else if (position.value < value) {
-      position.right = this.deleteNode(value, position.right)
-    }
-
-    if (position.value === value) {
-      if (position.left === null && position.right === null) {
-        return position = null
-      }
-      if (position.left === null) {
-        return position.right
-      } else if (position.right === null) {
-        return position.left
+    if (node.value === value) {
+      // If leaf node then simply delete
+      if (node.left === null && node.right === null) {
+        return (node = null);
       }
 
+      node;
+      // If only one child then copy the value of the non-null child to current node & delete child.
+      if (node.right !== null && node.left === null) {
+        node.value = node.right.value;
+        node.right = null;
+      } else if (node.left !== null && node.right === null) {
+        node.value = node.left.value;
+        node.left = null;
+      }
+
+      // Two children
+      if (node.left !== null && node.right !== null) {
+        // Get the inorder successor value
+        let successor = this.findInorderSucc(node.right).value;
+        // Delete the inorder successor
+        this.deleteNode(successor);
+        // Copy the value to the node
+        node.value = successor;
+      }
     }
 
-    return position
+    if (value > node.value) {
+      node;
+      node.right = this.deleteNode(value, node.right);
+    } else if (value < node.value) {
+      node.left = this.deleteNode(value, node.left);
+    }
+
+    return node;
   }
 
+  findInorderSucc(node) {
+    if (!node.left) return node;
+    return this.findInorderSucc(node.left);
+  }
 
-  find(search: number, position?: TreeNode | null): TreeNode | null {
-
-    if (position === undefined) {
-      position = this.root
+  find(search: number, node?: TreeNode | null): TreeNode | null {
+    if (node === undefined) {
+      node = this.root;
     }
 
-    if (position === null) {
-      return null
+    if (node === null) {
+      return null;
     }
 
-    if (position.value === search) {
-      return position
+    if (node.value === search) {
+      return node;
     }
 
-    if (position.value > search) {
-      return this.find(search, position.left)
-    } else if (position.value < search) {
-      return this.find(search, position.right)
+    if (node.value > search) {
+      return this.find(search, node.left);
+    } else if (node.value < search) {
+      return this.find(search, node.right);
     }
+    return null
+
   }
 }
 
-
-const tree = new Tree([1, 2, 3, 4, 5, 6, 7])
+const tree = new Tree([1, 2, 3, 4, 5, 6, 7]);
